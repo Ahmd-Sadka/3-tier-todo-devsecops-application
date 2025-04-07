dbd627deb9a31d1dd116b270eaf42573c45b7883
Ahmd-Sadka <ahmd.sadkaa@gmail.com>
2025-04-07 12:03:05 -0400

    update url

--- todos-infra/modules/pipelineModule/AMI.tf
+++ todos-infra/modules/pipelineModule/AMI.tf
@@ -1,10 +1,11 @@
 data "aws_ami" "jenkins" {
     
   most_recent = true
-  owners      = ["amazon"]
+  owners      = ["099720109477"]
   filter {
     name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-  
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+ }
+
 }
+  
